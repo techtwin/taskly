312cbfa78722a615b016f0f0e18b92da0a24030c
techtwins <gshevaughn@gmail.com>
2021-01-31 22:45:13 -0500

    styled cards and changed font type / styling for h1 - h5 tags

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import '../styles/card.css'
 
 export default function Card({ taskObj }) {
 
   return (
     <div className="card">
+      <div className="cardBtns">
+        <button style={{
+          backgroundColor: "transparent",
+          border: "none",
+          marginTop: "15px",
+          transform: "scale(0.7)"
+        }}>
+          <img src="./Move.png" alt="delete button"/>
+        </button>
+      </div>
       <div className="cardContent">
         <h1>{taskObj.name}</h1>
         <span>{taskObj.date}</span> - <span>{taskObj.time}</span><br /><br />
