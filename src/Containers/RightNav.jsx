a642864bc8a7ddc002632b08da2516102c9e074c
techtwins <gshevaughn@gmail.com>
2021-01-31 19:45:43 -0500

    refactored styling on github div

--- src/Containers/RightNav.jsx
+++ src/Containers/RightNav.jsx
@@ -4,7 +4,6 @@ import '../styles/rightnav.css'
 export default function RightNav() {
   return (
     <div className="rightNav">
-      Right Nav
       <div className="githubDiv">
         <h1 className="githubTitle">Checkout my Github here.</h1>
         <img className="gitman" src="./gitman.png" alt="avatar" />
