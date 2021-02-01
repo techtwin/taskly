b125dfe22e0927572a34ac7678f46ac409a4a038
techtwins <gshevaughn@gmail.com>
2021-01-31 19:16:43 -0500

    logo added

--- src/Containers/LeftNav.jsx
+++ src/Containers/LeftNav.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 export default function LeftNav() {
   return (
-    <div>
-      Left Nav
+    <div className="leftCont">
+      <img style={{ marginTop: "10px", marginLeft: "40px", marginBottom: "50px"}} src="./Logo.png" alt="logo" />
     </div>
   )
 }
