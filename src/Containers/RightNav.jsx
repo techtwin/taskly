8f88e5162629fdc7c4c1bd770b600be8c76a3a92
techtwins <gshevaughn@gmail.com>
2021-01-31 19:21:30 -0500

    github div with link implemented

--- src/Containers/RightNav.jsx
+++ src/Containers/RightNav.jsx
@@ -5,6 +5,13 @@ export default function RightNav() {
   return (
     <div className="rightNav">
       Right Nav
+      <div className="githubDiv">
+        <h1 className="githubTitle">Checkout my Github here.</h1>
+        <img className="gitman" src="./gitman.png" alt="avatar" />
+        <a role="button" href="https://github.com/techtwins" target="_blank" rel="noreferrer" className="gitmanBtn">
+          <img src="./github-btn.png" alt="github link" />
+        </a>
+      </div>
     </div>
   )
 }
