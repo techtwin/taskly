99e727a932596bab2fbd3bac5bb340acfb978d30
techtwins <gshevaughn@gmail.com>
2021-02-04 14:18:55 -0500

    moved login action to redux folder

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,8 +1,6 @@
 import React, { useState } from 'react'
 import { useDispatch } from 'react-redux'
-import { logIn } from '../redux/user';
-
-const url = "http://localhost:3000/";
+import { login } from '../redux/user';
 
 export default function Login() {
   const [state, setState] = useState({ username: "", password: "" })
@@ -16,37 +14,7 @@ export default function Login() {
 
   const submitHandler = (e) => {
     e.preventDefault()
-    if (state === undefined) {
-      const userDataStr = localStorage.getItem("USER_DATA");
-      let userDataObj = JSON.parse(userDataStr);
-      if (userDataObj) {
-        console.log("user data from local storage", userDataObj);
-        const action = logIn(userDataObj)
-        dispatch(action);
-      }
-      return;
-    }
-    fetch(`${url}users/login`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Accepts: "application.json",
-      },
-      body: JSON.stringify(state),
-    })
-      .then(r => r.json())
-      .then(data => {
-        if (data.id) {
-          console.log("found user", data.username);
-          localStorage.setItem("USER_DATA", JSON.stringify(data));
-          const action = logIn(data)
-          dispatch(action);
-        } else {
-          console.log("user not found");
-          window.alert("Wrong Username or Password Please Try Again");
-        }
-      })
-    .catch(console.log)
+    dispatch(login(state))
   }
   
   return (
--- src/redux/user.jsx
+++ src/redux/user.jsx
@@ -1,4 +1,5 @@
 import { createSlice } from '@reduxjs/toolkit'
+const url = "http://localhost:3000/";
 
 const userSlice = createSlice({
   name: "user",
@@ -17,4 +18,40 @@ const userSlice = createSlice({
 })
 
 export const { logIn, signUp, logOut } = userSlice.actions
+
+export const login = (userObj) => {
+  return function (dispatch) {
+    if (userObj === undefined) {
+      const userDataStr = localStorage.getItem("USER_DATA");
+      let userDataObj = JSON.parse(userDataStr);
+      if (userDataObj) {
+        console.log("user data from local storage", userDataObj);
+        const action = logIn(userDataObj)
+        dispatch(action);
+      }
+      return;
+    }
+    fetch(`${url}users/login`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accepts: "application.json",
+      },
+      body: JSON.stringify(userObj),
+    })
+      .then(r => r.json())
+      .then(data => {
+        if (data.id) {
+          console.log("found user", data.username);
+          localStorage.setItem("USER_DATA", JSON.stringify(data));
+          const action = logIn(data)
+          dispatch(action);
+        } else {
+          console.log("user not found");
+          window.alert("Wrong Username or Password Please Try Again");
+        }
+      })
+    .catch(console.log)
+  }
+}
 export default userSlice.reducer
