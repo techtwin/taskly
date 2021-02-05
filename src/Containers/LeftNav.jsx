bd233a73d96022b14f88a9e3f2af97ada21cff09
techtwins <gshevaughn@gmail.com>
2021-02-04 21:08:38 -0500

    installed momentjs

--- src/Containers/LeftNav.jsx
+++ src/Containers/LeftNav.jsx
@@ -1,6 +1,20 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { fetchAllTasks } from '../redux/task'
+import { Calendar, momentLocalizer } from 'react-big-calendar'
+import moment from 'moment'
+import "react-big-calendar/lib/css/react-big-calendar.css"
+import '../styles/leftnav.css'
 
 export default function LeftNav() {
+
+  const tasks = useSelector(({ tasks }) => tasks.tasks)
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    dispatch(fetchAllTasks())
+  }, [dispatch])
+
   return (
     <div className="leftCont">
       <img style={{ marginTop: "10px", marginLeft: "40px", marginBottom: "50px"}} src="./Logo.png" alt="logo" />
