0184275ef2aa1b5855e67e3e5490ea60982b2c0f
techtwins <gshevaughn@gmail.com>
2021-02-11 00:30:38 -0500

    new card to show received friend requests

--- src/Containers/RightNav.jsx
+++ src/Containers/RightNav.jsx
@@ -47,6 +47,15 @@ export default function RightNav() {
           </div>
           <span className="mySettings">My settings</span>
           <button className="logoutBtn" onClick={logout}>Logout</button>
+          <div className="addFriendDiv">
+            <h1 className="addFriendH1">Received Requests</h1>
+            {currentUser.requests_received.map(friend => (
+              <div style={{ display: "flex", justifyContent: "center" }}>
+                <h4 style={{ display: "inline-block", marginTop: "50px", marginLeft: "20px"}}>{friend.name}</h4>
+                <img style={{ objectFit: "contain", transform: "scale(0.3)", borderRadius: "46px" }} src={friend.img} alt="friend img"/>
+              </div>
+            ))}
+          </div>
           <div className="githubDiv">
             <h1 className="githubTitle">Checkout my Github here.</h1>
             <img className="gitman" src="./gitman.png" alt="avatar" />
@@ -55,16 +64,33 @@ export default function RightNav() {
             </a>
           </div>
           {/* friend bar */}
-          <div style={{ borderRadius: "20px", marginLeft: "550px", marginTop: "-755px", backgroundColor: "#f6f7fb93", height: "100%", width: "140px"}}>
+          <div style={{
+            position: "absolute",
+            top: 0,
+            right: 0,
+            borderRadius: "20px",
+            backgroundColor: "#f6f7fb93", 
+            height: "100%", 
+            width: "140px"
+          }}>
             <br />
-            <img style={{ marginTop: "-150px", marginLeft: "-245px", transform: "scale(0.25"}} src="./group-friends.jpg" alt="friend group"/>
-            <h1 style={{ backgroundColor: "black", color: "white", marginTop: "-100px", fontSize: "35px", fontWeight: 800}}>Friends</h1>
+            <img style={{ marginTop: "-150px", marginLeft: "-245px", transform: "scale(0.25)" }} src="./group-friends.jpg" alt="friend group"/>
+            <h1 style={{ 
+              backgroundColor: "black", 
+              color: "white", 
+              marginTop: "-100px", 
+              fontSize: "30px", 
+              fontWeight: 800, 
+              padding: "3px 0px"
+            }}>
+              Friends
+            </h1>
             {/* <> */}
               {currentUser.requests_sent.map(friend => (
                 <div style={{ alignItems: "center", marginBottom: "20px", paddingLeft: "10px", paddingRight: "10px"}}>
                   <br />
                   <h2 style={{ marginTop: "30px", fontSize: "22px", fontWeight: "800"}}>{friend.name}</h2>
-                  <img style={{ transform: "scale(0.9)", borderRadius: "46px" }} src={friend.img} alt="friends"></img>
+                  <img style={{ transform: "scale(0.8)", borderRadius: "46px" }} src={friend.img} alt="friends"></img>
                 </div>
               ))}
             {/* </> */}
